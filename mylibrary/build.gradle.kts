388dd62afe80f69ccac29a804a51937863c27867
LBARRIOS <luisabarrios01@gmail.com>
2025-08-13 14:18:42 -0300

    another commit

--- mylibrary/build.gradle.kts
+++ mylibrary/build.gradle.kts
@@ -30,11 +30,31 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "17"
+    }
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+        }
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("release") {
+            // podés cambiarlo si querés que el artifact se llame distinto
+            artifactId = "mylibrary"
+
+            // importante: publicar el AAR del variant 'release'
+            // (el afterEvaluate evita timing issues con AGP)
+            afterEvaluate {
+                from(components["release"])
+            }
+        }
     }
 }
 
